5056d637d8b94a50bfcbf95b3c99980dee346898
ETSGitHub <etschulte@gmail.com>
2018-07-25 16:25:43 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -7,12 +7,12 @@ def add_a_key_value_pair
   summer_olympics[:Atlanta] = "1996"
 end
 
-def iterate_through_hash
-  count = 0
-  summer_olympics.each do |country, year|
-    puts "The #{year} summer olympics took place in #{country}."
-  count += 1
-end
-end
+# def iterate_through_hash
+#   count = 0
+#   summer_olympics.each do |country, year|
+#     puts "The #{year} summer olympics took place in #{country}."
+#   count += 1
+# end
+# end
 
 
