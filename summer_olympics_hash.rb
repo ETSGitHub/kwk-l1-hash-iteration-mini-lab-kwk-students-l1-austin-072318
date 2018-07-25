89380f1e37b4b9e92ac5a8da9541e60de78a0bf1
ETSGitHub <etschulte@gmail.com>
2018-07-25 16:36:45 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -6,6 +6,7 @@ end
 def add_a_key_value_pair
   summer_olympics = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
   summer_olympics[:Atlanta] = "1996"
+  summer_olympics
 end
 
 def iterate_through_hash
