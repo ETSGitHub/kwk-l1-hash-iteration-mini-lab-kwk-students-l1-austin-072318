5e6d857715533315a4ab6a30ea5c9ef02e3d63d2
ETSGitHub <etschulte@gmail.com>
2018-07-25 16:34:04 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,22 +1,24 @@
 
-#def create_olympics_hash
+def create_olympics_hash
   summer_olympics = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
-#end
+end
 
-#def add_a_key_value_pair
+def add_a_key_value_pair
   summer_olympics[:Atlanta] = "1996"
-#end
+end
 
-# def iterate_through_hash
+def iterate_through_hash
    count = 0
    summer_olympics.each do |country, year|
      puts "The #{year} summer olympics took place in #{country}."
    count += 1
  end
-# end
+end
 
-#def iterate_through_keys
+def iterate_through_keys
   summer_olympics.each_key{|country| puts country.upcase}
-#end
+end
 
+iterate_through_hash
+iterate_through_keys
 
